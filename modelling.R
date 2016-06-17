deb8f83dc23aca6faa198e91e6016e37bec22b36
evanmiller29 <evanmiller29@gmail.com>
2016-06-17 12:06:49 +1200

    Beta PDF
    Adding in Beta PDF to determine whether given observations are drawn
    from which distribution (given sample values).

--- modelling.R
+++ modelling.R
@@ -1,7 +1,10 @@
 library(MASS)
 library(ggplot2)
+library(dplyr)
 
 #### Reading in the data
+
+op <- options(digits = 3)
 
 rm(list = ls())
 
@@ -24,20 +27,57 @@ set.seed(737)
 #### sd = population variance
 #### mixing proportion = 0.5
 
+pop = as.matrix(data)
 
-row_sample_1 <- sample(nrow(data),size=1,replace=TRUE)
-row_sample_2 <- sample(nrow(data),size=1,replace=TRUE)
+#### Starting points are as below. Will use K means to generate more reasonable estimates in future
 
-m1 <- data[row_sample_1, ]
-m2 <- data[row_sample_2, ]
-sd1 <- var(data)
-sd2 <- var(data)
+alpha1 <- 5
+beta1 <- 1
+alpha2 <- 10
+beta2 <- 5
 pi <- 0.5
 
-init <- c(m1, m2, sd1, sd2, pi)
+init <- c(alpha1, beta1, alpha2, beta2, pi)
 
-## avoid spurious accuracy
-op <- options(digits = 3)
+initial <- pop[1]
+
+comp_resp <- function(x, init_params){
+  
+  ### x: the observations for which responsibilties will be calculated
+  ### init_params: the initial values for the beta distributions
+  ###             1 = alpha parameter for first dist
+  ###             2 = beta parameter for first dist
+  ###             3 = alpha parameter for second dist
+  ###             4 = beta parameter for second dist
+  ###             5 = the class split
+  
+  alpha1 <- init_params[1]
+  beta1 <- init_params[2]
+  alpha2 <- init_params[3]
+  beta2 <- init_params[4]
+  pi <- init_params[5]
+
+  resp <- NULL
+  
+  for (i in 1:length(x)){
+    
+    initial_pop1[i] <- pbeta(x[i], alpha1, beta1, ncp = 0, lower.tail = TRUE, log.p = FALSE)
+    initial_pop2[i] <- pbeta(x[i], alpha2, beta2, ncp = 0, lower.tail = TRUE, log.p = FALSE)
+      
+    resp[i] = (pi * initial_pop2[i]) / ((1 - pi) * initial_pop1[i] + pi * initial_pop2[i])
+    
+    #### Now want to add bernoulli random draws to split into two classes
+    #### Iterate over these draws 8000-10000 times to get median parameters for next iteration
+    #### Keep going until we get convergence
+  }
+  
+  return(resp)
+}
+
+first_results <- comp_resp(pop, init)
+
+data.frame(resp = first_results) %>%
+    ggplot(., aes(x = resp)) + geom_density()
 
 #### Psuedo code
 #### 1. Use pi to randomly determine which dist a given sample is from
@@ -48,43 +88,3 @@ op <- options(digits = 3)
 #### should keep doing it until you get a result
 #### 4. Iterate until convergence of pi and mu/sd
 #### 5. After convergence return the parameters for each of the distributions
-
-mixture_model <- function(initial_params, X){
-  
-  ### initial_params : a list of variables holding first estimates of mu, sd and pi
-  ### data : the population from which the underlying subpopulations are being investigated
-  ### returns : TBC
-  
-  pop <- as.matrix(X)
-  rows <- nrow(pop)
-  
-  elements_1 <- pi * rows
-  elements_2 <- (1 - pi) * rows
-  
-  pop1 <- sample(pop, elements_1, replace=FALSE)
-  pop2 <- setdiff(pop, pop1)
-      
-}
-
-list[pop1, pop2] <- mixture_model(init, data)
-length(pop)
-### initial_params : a list of variables holding first estimates of mu, sd and pi
-### data : the population from which the underlying subpopulations are being investigated
-### returns : TBC
-
-pop <- as.matrix(data)
-rows <- nrow(pop)
-
-elements_1 <- pi * rows
-elements_2 <- (1 - pi) * rows
-
-pop1 <- sample(pop, elements_1, replace=FALSE)
-pop2 <- pop[sample(pop, elements_2, replace=TRUE), ]
-
-  
-head(pop)
-
-
-nrow(pop)
-
-sample(data, pi * nrow(data), replace=TRUE)
